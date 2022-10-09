35a5f852a1f4956a3a12849ca13af78503337f37
Shportko <iurii.shportko@gmail.com>
2022-10-09 16:58:08 -0500

    refresh

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ function getDataFromServer(number) {
   };
 }
 window.onload = function () {
-  getDataFromServer(2);
+  getDataFromServer(10);
 };
 
 function buildStudentsTable(students) {
   const tableBody = document.getElementById("students-table-tbody");
   tableBody.innerHTML = null;
   console.log(tableBody);
-
+  
   if (students && students.length > 0) {
     for (let i = 0; i < students.length; i++) {
       const student = students[i];
@@ -26,15 +26,20 @@ function buildStudentsTable(students) {
       const row = document.createElement("tr");
       row.id = student.id;
 
-      row.addEventListener("click", () => {});
+      row.addEventListener("click", () => {
+        numberCell.classList = ['search-txt'];
+        buildStudentsProfile(student);
+      });
+    
 
       const numberCell = document.createElement("td");
       numberCell.innerHTML = i + 1;
       const firstNameCell = document.createElement("td");
       firstNameCell.innerHTML = student.firstName;
+      firstNameCell.classList = ['td']
       const lastNameCell = document.createElement("td");
       lastNameCell.innerHTML = student.lastName;
-      const ageCell = document.createElement("td");
+      const ageCell = document.createElement("td"); 
       ageCell.innerHTML = student.age;
       const genderCell = document.createElement("td");
       genderCell.innerHTML = student.gender;
@@ -65,21 +70,51 @@ function buildStudentsProfile(student) {
   if (student) {
     const studentImg = document.createElement("img");
     studentImg.alt = "studentImage";
-    studentImg.src = "student.profileImageUrl";
+    studentImg.src = student.profileImageUrl;
     studentImg.style.width = "200px";
     studentImg.style.height = "200px";
     studentImg.style.objectFit = "cover";
 
-    const studentName = `${student.firstName} ${student.lastName}`;
+    const studentName = `<b>STUDENT INFORMATION:</b> <br> First name: ${student.firstName} <br> Last name: ${student.lastName} <br> Age: ${student.age} <br> Gender: ${student.gender} <br> Has car: ${student.hasCar}`;
     const nameContainer = document.createElement("div");
+    nameContainer.classList = ['divdiv'];
     nameContainer.innerHTML = studentName;
     studentProfileContainer.appendChild(nameContainer);
+    studentProfileContainer.appendChild(studentImg);
   } else {
     studentProfileContainer.innerHTML = "No data";
   }
+
 }
 
-/*document.querySelector('#search-txt').oninput = function() {
-    let val = this.value.trim();
-    let elasticItems = document.querySelectorAll('.search-txt td');
-}*/
+// ----------------------------------search
+
+document.querySelector('#elastic').oninput = function () {
+  let val = this.value.trim();
+  let elasticItems = document.querySelectorAll('.elastic td');
+          if (val != '') {
+              elasticItems.forEach(function(elem) {
+                  if (elem.innerText.search(val) == - 1) {
+                  let remove = elem.classList.add("hide");
+                  elem.innerHTML = elem.innerText;
+              }
+          else {
+                   elem.classList.remove("hide");
+                   let str = elem.innerText;
+                   elem.innerHTML = insertMark (str,elem.innerText.search(val),val.length);
+              }
+      });                 
+  } 
+  else {
+      elasticItems.forEach (function (elem) {
+      let remove = elem.classList.remove("hide");
+      elem.innerHTML = elem.innerText;
+      });
+  }  
+}
+
+function insertMark(string,pos,len) {
+return string.slice(0, pos) + '<mark>' + string.slice(pos, pos + len) + '</mark>' + string.slice(pos + len);
+}
+
+
